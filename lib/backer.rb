e72e1884cb4da2c6db9cf69ad5b47c2cbb5dd9b9
Elronia <zar.catherine@gmail.com>
2020-08-09 22:42:01 -0400

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,17 @@
 require_relative '../lib/project'
 require_relative '../lib/project_backer'
+
 class Backer
     attr_reader :name
+
     def initialize(name)
         @name = name
     end
+
     def back_project(project)
         ProjectBacker.new(project, self)
     end
+
     def backed_projects
         project_array = ProjectBacker.all.select do |project_list|
             project_list.backer == self
@@ -16,4 +20,5 @@ class Backer
             project_list.project
         end
     end
+    
 end
